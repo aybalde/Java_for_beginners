904c492ffcb42e81f8258a3aa7d665d08e91c2f4
aybalde <alphayayabaldetoke@gmail.com>
2022-11-23 19:55:57 +0100

    methode

--- 3. Objects and Methods/Methods/3.6 Counting Sheep/src/Sheep.java
+++ 3. Objects and Methods/Methods/3.6 Counting Sheep/src/Sheep.java
@@ -1,6 +1,7 @@
-public class Sheep
-{
+public class Sheep {
     static int count; //don't set as private for internal reasons
 
-    /* Add 1 to the Sheep count everytime one is created. */
+    public Sheep() {
+        count++;
+    }
 }
--- 4. Arrays and String Methods/String Methods/4.1 Get Shortest Word/src/GetShortestWord.java
+++ 4. Arrays and String Methods/String Methods/4.1 Get Shortest Word/src/GetShortestWord.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 public class GetShortestWord
 {
-    /* Code a method that returns the shortest word of the two inputs*/
+    public String word1;
+    public String word2;
+    public GetShortestWord(String wor1, String wor2){
+        word1 = wor1;
+        word2 = wor2;
+    }
+   public static String getShortestWord(String word1, String word2){
+       if (word1.length() < word2.length()){
+           return word1;
+       }
+       else if(word1.length() > word2.length()) {
+           return word2;
+       }
+       else
+           return word1;
+   }
 }
